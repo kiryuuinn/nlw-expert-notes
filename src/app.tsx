a3fa50ca88340ac8d1acea540226adb673d9b897
kiryuuinn <anderson.yoshimi.contato@gmail.com>
2024-02-06 00:50:28 -0300

    logo-nlw-expert

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,6 @@ export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert Notes" />
-
       <form className="w-full">
         <input
           type="text"
@@ -14,9 +13,7 @@ export function App() {
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
         />
       </form>
-
       <div className="h-px bg-slate-700" />
-
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
         <NoteCard />
